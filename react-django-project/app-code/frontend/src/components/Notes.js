b491e81c98cf46187e67114ed0b7abd78463ebaf
Asish <apatro917@gmail.com>
2025-11-11 00:28:04 +0530

    fix

--- react-django-project/app-code/frontend/src/components/Notes.js
+++ react-django-project/app-code/frontend/src/components/Notes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from '../axios';
 import { TextField, Button, Typography, Container, Box, Grid, Card, CardContent, CardActions, Paper } from '@mui/material';
 
 const Notes = () => {
